1cd8c3d3c72670236dc19ce4c8293fbb0bb080fa
SHS <69961510+quf265@users.noreply.github.com>
2024-01-24 18:38:17 +0900

    Create bcc_install_20.sh

---
+++ bcc_install_20.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#20version
+sudo apt install -y zip bison build-essential cmake flex git libedit-dev \
+  libllvm12 llvm-12-dev libclang-12-dev python zlib1g-dev libelf-dev libfl-dev python3-setuptools \
+  liblzma-dev arping netperf iperf
+
+git clone https://github.com/iovisor/bcc.git
+mkdir bcc/build; cd bcc/build
+cmake ..
+make
+sudo make install
+cmake -DPYTHON_CMD=python3 .. # build python3 binding
+pushd src/python/
+make
+sudo make install
+popd
